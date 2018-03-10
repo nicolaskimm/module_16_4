e8f47402abc7d90c0bec1922ad3e89abf53fd4e2
Joanna Krężołek <j.krezolek@gmail.com>
2018-03-10 18:36:31 +0100

    implement title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = props => {
+        <div>
+            <h1>Your ToDo App</h1>
+            <p>Number of tasks: {props.amount}</p>
+        </div>
+}
+
+
+export default Title;
