45b343c9c4d2f8a751908cd535681761eb8e8bb4
Joanna Krężołek <j.krezolek@gmail.com>
2018-03-10 18:39:27 +0100

    fix indentations

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Title = props => {
-        <div>
-            <h1>Your ToDo App</h1>
-            <p>Number of tasks: {props.amount}</p>
-        </div>
+    <div>
+        <h1>Your ToDo App</h1>
+        <p>Number of tasks: {props.amount}</p>
+    </div>
 }
 
 
