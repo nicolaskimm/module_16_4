41100f8109d2a71bfecca029f6f5794fbc225cd2
nicolaskimm <32582359+nicolaskimm@users.noreply.github.com>
2018-03-11 16:58:25 +0100

    add return

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Title = props => {
-    <div>
-        <h1>Your ToDo App</h1>
-        <p>Number of tasks: {props.amount}</p>
-    </div>
+    return (
+        <div>
+            <h1>Your ToDo App</h1>
+            <p>Number of tasks: {props.amount}</p>
+        </div>
+    )
 }
 
 
